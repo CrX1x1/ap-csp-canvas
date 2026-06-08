dc062c3826c509ed795392c5cf9c4b129ee61d15
Qi <qixu140@gmail.com>
2026-06-08 14:38:22 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,39 +2,54 @@ let shared;
 let me;
 let others;
 let picker;
+let showPicker = false;
+let title;
+
 function preload() {
     partyConnect("wss://demoserver.p5party.org", "drawingthing");
     shared = partyLoadShared("globals", {allStrokes:[]});
-    me = partyLoadMyShared({ x: 200, y: 200 , r: 10, hue:0, sat:100, br: 100, currentStroke:[]});
-    others = partyLoadGuestShareds()
+    me = partyLoadMyShared({ x: 200, y: 200 , r: 10, hue: 0, sat: 100, br: 100, currentStroke:[]});
+    others = partyLoadGuestShareds();
 }
 
 function setup() {
-  colorMode(HSB)
-  picker = createColorPicker(color(me.hue, me.sat, me.br))
-  picker.position(0,0)
-  pickerLabel = createElement("p", "<== color picker")
-  pickerLabel.position(50,-10)
+
+  colorMode(HSB, 360, 100, 100);
   createCanvas(6000, 4000);
   background(220);
+  
   if (!shared.allStrokes) {
     shared.allStrokes = [];
   }
+
+  picker = createColorPicker('#ff0000'); 
+  picker.position(20, 20);
+  picker.style('position', 'fixed');
+  picker.hide(); 
 }
 
 function draw() {
   background(220);
+  
+  if (showPicker) {
+    let c = picker.color();
+    me.hue = hue(c); 
+    me.sat = saturation(c);
+    me.br = brightness(c);
+  }
+  
   stroke(me.hue, me.sat, me.br);
-  strokeWeight(me.r/2)
-  circle(mouseX, mouseY, me.r/2);
-  me.x = mouseX
-  me.y = mouseY
+  strokeWeight(me.r / 2);
+  circle(mouseX, mouseY, me.r / 2);
+  me.x = mouseX;
+  me.y = mouseY;
+
   for (let i = 0; i < shared.allStrokes.length; i++) {
     let allSegments = shared.allStrokes[i];
     for (let j = 0; j < allSegments.length; j++) {
         let seg = allSegments[j];
         strokeWeight(seg[4]);
-        stroke(seg[5],seg[6],seg[7]);
+        stroke(seg[5], seg[6], seg[7]);
         line(seg[0], seg[1], seg[2], seg[3]);
     }
   }
@@ -42,25 +57,42 @@ function draw() {
   if (mouseIsPressed) {
     let vectorSegment = [pmouseX, pmouseY, mouseX, mouseY, me.r, me.hue, me.sat, me.br];
     me.currentStroke.push(vectorSegment);
+    
     for (let j = 0; j < me.currentStroke.length; j++) {
         let seg = me.currentStroke[j];
         strokeWeight(seg[4]);
-        stroke(seg[5],seg[6],seg[7]);
+        stroke(seg[5], seg[6], seg[7]);
         line(seg[0], seg[1], seg[2], seg[3]);
     }
   }
   
-  for (const o of others){
-    stroke(o.hue,o.sat,o.br)
-    strokeWeight(o.r/2)
-    circle(o.x, o.y, o.r/2);
-    if (o.currentStroke){
-      for (let i = 0; i < o.currentStroke.length; i++){
-          let seg = o.currentStroke[i]
+  for (const o of others) {
+    stroke(o.hue, o.sat, o.br);
+    strokeWeight(o.r / 2);
+    circle(o.x, o.y, o.r / 2);
+    
+    if (o.currentStroke) {
+      for (let i = 0; i < o.currentStroke.length; i++) {
+          let seg = o.currentStroke[i];
           strokeWeight(seg[4]);
+          stroke(seg[5], seg[6], seg[7]);
           line(seg[0], seg[1], seg[2], seg[3]);
       }
     }
+  }
+}
+
+function keyPressed() {
+  if (keyCode === TAB) {
+    showPicker = !showPicker; 
+    
+    if (showPicker) {
+      picker.show(); 
+    } else {
+      picker.hide(); 
+    }
+    
+    return false;
   }
 }
 
@@ -70,26 +102,26 @@ function mouseWheel(event) {
         me.hue += 5;
         if (me.hue > 360) me.hue = 0;
     } else if (keyIsDown(CONTROL)) {
-      me.sat += 3;
+        me.sat += 3;
     } else if (keyIsDown(ALT)) {
-      me.br += 3
+        me.br += 3;
     } else {
-      if (me.r < 25) { me.r++; }
+        if (me.r < 25) { me.r++; }
     }
   } else {
     if (keyIsDown(SHIFT)) {
         me.hue -= 5;
         if (me.hue < 0) me.hue = 360;
     } else if (keyIsDown(CONTROL)) {
-      me.sat -= 3;
+        me.sat -= 3;
     } else if (keyIsDown(ALT)) {
-      me.br -= 3
+        me.br -= 3;
     } else {
         if (me.r > 1) { me.r--; }
     }
   }
-  me.sat = constrain(me.sat, 0, 100)
-  me.br = constrain(me.br, 0, 100)
+  me.sat = constrain(me.sat, 0, 100);
+  me.br = constrain(me.br, 0, 100);
   return false;
 }
 
@@ -101,5 +133,5 @@ function mouseReleased() {
   if (me.currentStroke.length > 0) {
     shared.allStrokes.push(me.currentStroke);
   }
-  me.currentStroke = []
+  me.currentStroke = [];
 }
